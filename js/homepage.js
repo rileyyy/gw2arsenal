caf1ab2b448bb5f4f129c1f4053651ae8d882154
Linsinbigler, Michael A <malinsinbigler@gmail.com>
2015-10-08 10:03:35 -0400

    Added content div, clicking on the navigation buttons will update the content

--- js/homepage.js
+++ js/homepage.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
 		$("#left_nav li").removeClass('nav_active');
 		$(this).addClass('nav_active');
 		//alert($(this).attr('id'));
+
+		//Add selected button id to content
+		$("#content").html($(this).attr('id'));
 	})
 });
