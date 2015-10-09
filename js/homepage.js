37eb19a458789e470acc5eafb285670776092a18
rileyyy <rileyyy@live.com>
2015-10-09 06:49:19 -0400

    dun made shit ugly. but yay for some $.get

--- js/homepage.js
+++ js/homepage.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
 		//alert($(this).attr('id'));
 
 		//Add selected button id to content
-		$("#content").html($(this).attr('id'));
+		//$("#content").html($(this).attr('id'));
+		if( $(this).attr('id') == 'characters_nav' )
+			$.get("https://api.guildwars2.com/v1/colors.json", function(data, status){
+				$('#content').html(JSON.stringify(data));
+			})	
+		else if( $(this).attr('id') == 'wvw_nav' )
+			$.get("https://api.guildwars2.com/v1/wvw/matches.json", function(data, status){
+				$('#content').html(JSON.stringify(data, null, "\t"));
+			})
+		else
+			$('#content').html($(this).attr('id'));
+		
+
 	})
 });
